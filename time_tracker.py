7bd0fb95f919516717574dfc4a1d538511840723
newn0w <samuelpetersonacs@gmail.com>
2023-11-04 06:00:33 -0400

    Added program skeleton and database file

---
+++ time_tracker.py
@@ -0,0 +1,17 @@
+import tkinter as ttk
+from tkinter import *
+
+# Initializes root window
+root = Tk()
+
+# Set root window title and dimensions
+root.title("Time Tracker")
+root.geometry('1280x700')
+
+# Insert contents here
+#
+#
+#
+#
+
+root.mainloop()
