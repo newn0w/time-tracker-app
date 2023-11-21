edcc1a12a4517601da29055f511087cee821e783
newn0w <samuelpetersonacs@gmail.com>
2023-11-21 07:12:16 -0500

    Added empty treeview table + basic functionality

--- time_tracker.py
+++ time_tracker.py
@@ -1,5 +1,5 @@
 import tkinter as ttk
-from tkinter import *
+from tkinter import Tk, ttk
 
 # Initializes root window
 root = Tk()
@@ -8,10 +8,24 @@ root = Tk()
 root.title("Time Tracker")
 root.geometry('1280x700')
 
-# Insert contents here
-#
-#
-#
-#
+# Configuring columns of root window in order to place treeview
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(2, weight=1)
+
+# Tree view for sorting and displaying process data
+columns = ("Process", "URL", "Time Logged")  # TODO: Add extra columns?
+tree = ttk.Treeview(root, columns=columns, show="headings", height=19)
+
+# Adjusting attributes of treeview
+tree.heading("Process", text="Process")  # Sets heading text for each column
+tree.heading("URL", text="URL")
+tree.heading("Time Logged", text="Time Logged")
+tree.column("Process", minwidth=0, width=420)  # Adjusts sizes of columns and treeview
+tree.column("URL", minwidth=0, width=420)
+tree.column("Time Logged", minwidth=0, width=420)
+tree.grid(row=2, column=0, columnspan=3, sticky='ew')  # Spans treeview across top 3 columns.
+
+# TODO: Add horizontal resizing functionality
 
 root.mainloop()
